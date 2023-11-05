48e99a13202aa17eda92b572332628e0711f6a1d
levizwannah <levizwannah@gmail.com>
2023-11-05 13:28:17 +0100

    finished lesson 8

---
+++ 8-loops.js
@@ -0,0 +1,26 @@
+// while loops
+// let counter = 0;
+
+// while(counter <= 10){
+//     console.log(counter); // loop-body
+//     counter++; // loop-body
+// }
+
+// // do-while loops
+// do {
+//     console.log(counter);
+//     // statements
+
+//     counter++;
+// }
+// while(counter <= 10);
+
+// for loops
+for(let counter = 5; counter <= 10; counter++) {
+    console.log(counter);
+}
+
+// javascript specific
+// for-in-loop
+
+// for-of-loop
